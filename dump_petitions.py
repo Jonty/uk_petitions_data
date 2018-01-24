3c51d56eb13f575300d8375fed184b4b6cc14904
Jonty Wareing <jonty@jonty.co.uk>
2018-01-24 20:03:25 +0000

    Add petitions from all governments

--- dump_petitions.py
+++ dump_petitions.py
@@ -1,15 +1,31 @@
+import os
 import json
 import requests
 
-url = 'https://petition.parliament.uk/petitions.json?state=all'
-while url:
-    print('Fetching %s...' % url)
-    response = requests.get(url)
-    data = json.loads(response.content)
+# The petitions API is totally broken when browsing old governments so we have
+# to fix up the URL
+governments = {
+    '2010-2015_Conservative_-_Liberal_Democrat_coalition_government': ('https://petition.parliament.uk/archived/petitions.json?state=all', '&parliament=2'),
+    '2015-2017_Conservative_government': ('https://petition.parliament.uk/archived/petitions.json?parliament=1&state=all', '&parliament=1'),
+    '2017-_Conservative_government': ('https://petition.parliament.uk/petitions.json?state=all', '')
+}
 
-    for petition in data['data']:
-        with open('petitions/%s.json' % petition['id'], 'w') as f:
-            print('Dumping %s' % petition['id'])
-            f.write(json.dumps(petition, sort_keys=True, indent=4, separators=(',', ': ')))
+for gov, (url, append) in governments.items():
+    print('Processing the %s government....' % gov)
+    path = 'petitions/' + gov
+    try:
+        os.mkdir(path)
+    except OSError:
+        pass
 
-    url = data['links']['next']
+    while url:
+        print('Fetching %s...' % (url + append))
+        response = requests.get(url + append)
+        data = json.loads(response.content)
+
+        for petition in data['data']:
+            with open('%s/%s.json' % (path, petition['id']), 'w') as f:
+                print('Dumping %s' % petition['id'])
+                f.write(json.dumps(petition, sort_keys=True, indent=4, separators=(',', ': ')))
+
+        url = data['links']['next']
